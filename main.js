6abeea134f5ccb890df8662c4500ee18a5bca40c
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-08-14 15:15:35 +0300

    added logic for the navbar

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('nav-window-scroll', window.scrollY > 0)
 })
 
-const faqs = document.querySelectorAll('.faq')
+//
 
+const faqs = document.querySelectorAll('.faq')
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
         faq.classList.toggle('open');
@@ -18,4 +19,22 @@ faqs.forEach(faq => {
             icon.className = 'uil uil-plus'
         }
     })
+})
+
+//
+
+const menu = document.querySelector(".nav__menu")
+const openMenuBtn = document.querySelector("#open-menu-btn")
+const closeMenuBtn = document.querySelector("#close-menu-btn")
+
+openMenuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeMenuBtn.style.display = 'inline-block';
+    openMenuBtn.style.display = 'none';
+})
+
+closeMenuBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    closeMenuBtn.style.display = 'none';
+    openMenuBtn.style.display = 'inline-block';
 })
